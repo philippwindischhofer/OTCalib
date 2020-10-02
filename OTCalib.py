80b1997c902ae06d2b18f9004b3d0d5b1b718ab3
Philipp Windischhofer <philipp.windischhofer@cern.ch>
2020-10-02 09:28:29 +0100

    toy example for simple transport between two Gaussians

--- OTCalib.py
+++ OTCalib.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-outprefix = 'test/'
 device='cpu'
 
 # set number of threads to e used by torch
@@ -30,62 +28,63 @@ from otcalibutils import *
 nbatches = 2**10
 nepochs = 200
 
+# some global settings for the logging
+outprefix = 'toy_example'
 
-decays = [0.98]
-acts = [("lrelu", nn.LeakyReLU)] #, ("sig", nn.Sigmoid), ("tanh", nn.Tanh)]
-bss = [64]
-npss = [4, 1]
-nlayer = [4]
-latent = [64]
-lrs = [(0.5, 5e-2)]
-dss = [int(1e6), int(1e5), int(1e4)]
+# some global settings for the dataset
+length_data = 10000
+length_MC = 10000
+
+# some global settings pertaining to the models
+activation = nn.LeakyReLU
+adversary_lr = 5e-3
+transport_lr = 1e-3
+lr_decay = 0.98
+
+# some global settings for the training
+batchsize = 1024
 
 controlplots(int(1e6))
-plt.savefig("controlplots.pdf")
 
-for (decay, (actname, activation), batchsize, nps, nlay, nlat, (alr, tlr), datasize) \
-  in product(decays, acts, bss, npss, nlayer, latent, lrs, dss):
+# -----------------------------------------------
+# start preparing the networks
+# -----------------------------------------------
 
+alldata = genData(length_data, device)
+allmc = genMC(length_MC, device)
 
-    alldata = genData(datasize, device)
-    allmc = genMC(4*datasize, device)
+transport = fullyConnected(number_layers = 2, number_inputs = 1, number_outputs = 1, hidden_units = 15, activation = activation)
+adversary = fullyConnected(number_layers = 2, number_inputs = 1, number_outputs = 1, hidden_units = 15, activation = activation)
 
-    transport = fullyConnected(nlay, 2, nlat, 1+nps, activation)
-
-    adversary = fullyConnected(nlay, 2, nlat*2, 1, nn.LeakyReLU)
-
-
-    transport.to(device)
-    adversary.to(device)
+transport.to(device)
+adversary.to(device)
     
-    toptim = torch.optim.SGD(transport.parameters(), lr=tlr)
-    aoptim = torch.optim.SGD(adversary.parameters(), lr=alr)
+toptim = torch.optim.SGD(transport.parameters(), lr = transport_lr)
+aoptim = torch.optim.SGD(adversary.parameters(), lr = adversary_lr)
 
-    tsched = torch.optim.lr_scheduler.ExponentialLR(toptim, decay)
-    asched = torch.optim.lr_scheduler.ExponentialLR(aoptim, decay)
+tsched = torch.optim.lr_scheduler.ExponentialLR(toptim, lr_decay)
+asched = torch.optim.lr_scheduler.ExponentialLR(aoptim, lr_decay)
 
+writer = SummaryWriter(outprefix)
 
-    name = \
-      "sgdexp_%.2f_act_%s_batch_%d_nps_%d_layers_%d_latent_%d_tlr_%0.2e_alr_%0.2e_datasize_%d" \
-        % (decay, actname, batchsize, nps, nlay, nlat, tlr, alr, datasize)
+# -----------------------------------------------
+# start the training
+# -----------------------------------------------
 
-    writer = SummaryWriter(outprefix + name)
+for epoch in range(nepochs):
+    radvloss = 0
+    fadvloss = 0
+    tadvloss = 0
+    ttransloss = 0
+    realavg = 0
+    fakeavg = 0
 
+    print("epoch:", epoch)
 
-    for epoch in range(nepochs):
-      radvloss = 0
-      fadvloss = 0
-      tadvloss = 0
-      ttransloss = 0
-      realavg = 0
-      fakeavg = 0
+    for batch in range(nbatches):
 
-      print("epoch:", epoch)
-
-      for batch in range(nbatches):
-
+        # sample from data and MC
         data = alldata[torch.randint(alldata.size()[0], size=(batchsize,), device=device)]
-
         mc = allmc[torch.randint(allmc.size()[0], size=(batchsize,), device=device)]
 
         toptim.zero_grad()
@@ -103,22 +102,11 @@ for (decay, (actname, activation), batchsize, nps, nlay, nlat, (alr, tlr), datas
 
         radvloss += tmp1.item()
 
+        # calibrate the MC with the current transport model
+        transporting = trans(transport, mc)
+        transported = transporting + mc
 
-        # add gradient regularization
-        # grad_params = torch.autograd.grad(tmp1, adversary.parameters(), create_graph=True, retain_graph=True)
-        # grad_norm = 0
-        # for grad in grad_params:
-        #     grad_norm += grad.pow(2).sum()
-        # grad_norm = grad_norm.sqrt()
-
-
-        thetas = torch.randn((batchsize, nps), device=device)
-        transporting = trans(transport, mc, thetas)
-
-        transported = transporting + mc[:,0:1]
-
-        fake = adversary(torch.cat([transported, mc[:,1:]], axis=1))
-
+        fake = adversary(transported)
         fakeavg += torch.mean(fake).item()
 
         tmp2 = \
@@ -130,23 +118,21 @@ for (decay, (actname, activation), batchsize, nps, nlay, nlat, (alr, tlr), datas
 
         fadvloss += tmp2.item()
 
-        loss = tmp1 + tmp2 # + 0.1*grad_norm
+        # total adversary loss
+        loss = tmp1 + tmp2
 
+        # train the adversary
         loss.backward()
         aoptim.step()
-
 
         toptim.zero_grad()
         aoptim.zero_grad()
 
-        thetas = torch.randn((batchsize, nps), device=device)
-        transporting = trans(transport, mc, thetas)
+        transporting = trans(transport, mc)
+        transported = transporting + mc
 
-        transported = transporting + mc[:,0:1]
-        fake = adversary(torch.cat([transported, mc[:,1:]], axis=1))
-
-        tmp1 = tloss(transporting)
-        ttransloss += tmp1.item()
+        
+        fake = adversary(transported)
 
         tmp2 =\
           binary_cross_entropy_with_logits(
@@ -155,42 +141,26 @@ for (decay, (actname, activation), batchsize, nps, nlay, nlat, (alr, tlr), datas
           , reduction='mean'
           )
 
+        # train the transport network to force the adversary output on transported MC to be one
+        # (i.e. the data label)
         tadvloss += tmp2.item()
-
-        loss = tmp2 # tmp1 + tmp2
+        loss = tmp2
 
         loss.backward()
         toptim.step()
 
+    # update the learning rates
+    tsched.step()
+    asched.step()
 
-      tsched.step()
-      asched.step()
+    # write tensorboard info once per epoch
+    writer.add_scalar('radvloss', radvloss / nbatches, epoch)
+    writer.add_scalar('fadvloss', fadvloss / nbatches, epoch)
+    writer.add_scalar('tadvloss', tadvloss / nbatches, epoch)
+    writer.add_scalar('ttransloss', ttransloss / nbatches, epoch)
+    writer.add_scalar('realavg', realavg / nbatches, epoch)
+    writer.add_scalar('fakeavg', fakeavg / nbatches, epoch)
 
-      # write tensorboard info once per epoch
-      writer.add_scalar('radvloss', radvloss / nbatches, epoch)
-      writer.add_scalar('fadvloss', fadvloss / nbatches, epoch)
-      writer.add_scalar('tadvloss', tadvloss / nbatches, epoch)
-      writer.add_scalar('ttransloss', ttransloss / nbatches, epoch)
-      writer.add_scalar('realavg', realavg / nbatches, epoch)
-      writer.add_scalar('fakeavg', fakeavg / nbatches, epoch)
-
-
-      # make validation plots once per epoch
-
-      plotPtTheta(transport, ptbin(25, 50, allmc), ptbin(25, 50, alldata), nps, writer, "pt_25_50", "$25 < p_T < 50$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(50, 75, allmc), ptbin(50, 75, alldata), nps, writer, "pt_50_75", "$50 < p_T < 75$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(75, 100, allmc), ptbin(75, 100, alldata), nps, writer, "pt_75_100", "$75 < p_T < 100$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(100, 150, allmc), ptbin(100, 150, alldata), nps, writer, "pt_100_150", "$100 < p_T < 150$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(150, 200, allmc), ptbin(150, 200, alldata), nps, writer, "pt_150_200", "$150 < p_T < 200$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(200, 300, allmc), ptbin(200, 300, alldata), nps, writer, "pt_200_300", "$200 < p_T < 300$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(300, 500, allmc), ptbin(300, 500, alldata), nps, writer, "pt_300_500", "$300 < p_T < 500$", epoch, device, nmax=250)
-
-      plotPtTheta(transport, ptbin(500, 1000, allmc), ptbin(500, 1000, alldata), nps, writer, "pt_500_1000", "$500 < p_T < 1000$", epoch, device, nmax=250)
-
-      save(outprefix + name + ".pth", transport, adversary, toptim, aoptim)
+    # make debug plots in each epoch to follow along
+    detailed_plots(transport, writer, epoch, device)
+    
--- otcalibutils.py
+++ otcalibutils.py
@@ -33,30 +33,27 @@ def discMC(xs, logpts):
 def discData(xs, logpts):
   return poly([0, 0.7, 1.1, 1.3], xs) * poly([1, -0.1, 0.02], logpts)
 
-
 def logptMC(xs):
   return torch.log(poly([25, 200, 7], -torch.log(xs)))
 
 def logptData(xs):
   return torch.log(poly([25, 220, 5], -torch.log(xs)))
 
-
-
-# need to add a small number to avoid values of zero.
 def genMC(n, device):
-  xs = torch.rand(n, device=device) + 1e-5
-  logpts = logptMC(xs)
-  ys = torch.rand(n, device=device) + 1e-5
-  ds = discMC(ys, logpts)
-  return torch.stack([ds, logpts]).transpose(0, 1)
+  
+  stdev = 1
+  mean = -2
+  samples = torch.randn(n, device = device) * stdev + mean
+
+  return torch.unsqueeze(samples, 1)
 
 def genData(n, device):
-  xs = torch.rand(n, device=device) + 1e-5
-  logpts = logptData(xs)
-  ys = torch.rand(n, device=device) + 1e-5
-  ds = discData(ys, logpts)
-  return torch.stack([ds, logpts]).transpose(0, 1)
 
+  stdev = 1
+  mean = 2
+  samples = torch.randn(n, device = device) * stdev + mean
+
+  return torch.unsqueeze(samples, 1)
 
 # give high-pT jets more weight to improve convergence
 # similar idea to boosting
@@ -74,34 +71,15 @@ def histcurve(bins, fills, default):
 
 
 def controlplots(n):
-  mc = genMC(n, 'cpu').numpy()
-  data = genData(n, 'cpu').numpy()
+  mc = genMC(n, 'cpu').numpy()[:, 0]
+  data = genData(n, 'cpu').numpy()[:, 0]
 
-
-  plt.figure(figsize=(30, 10))
-
-  plt.subplot(1, 3, 1)
-
-  _ = plt.hist([np.exp(mc[:,1]), np.exp(data[:,1])], bins=25, label=["mc", "data"])
-  plt.title("pT")
-  plt.yscale("log")
-  plt.legend()
-
-  plt.subplot(1, 3, 2)
-
-
-  _ = plt.hist([mc[:,1], data[:,1]], bins=25, label=["mc", "data"])
-  plt.title("log pT")
-  plt.legend()
-
-  plt.subplot(1, 3, 3)
-
-  _ = plt.hist([mc[:,0], data[:,0]], bins=25, label=["mc", "data"])
-  plt.title("discriminant")
-  plt.legend()
-  plt.show()
-
-# test(int(1e6))
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  
+  _ = ax.hist([mc, data], bins=25, label=["mc", "data"])
+  ax.legend()
+  fig.savefig("controlplots.pdf")
 
 
 def layer(n, m, act):
@@ -120,13 +98,13 @@ def sequential(xs):
     return nn.Sequential(d)
 
 
-def fullyConnected(nl, n0, nmid, nf, act):
+def fullyConnected(number_layers, number_inputs, number_outputs, hidden_units, activation):
   return \
     nn.Sequential(
-      nn.Linear(n0, nmid)
-    , act(inplace=True)
-    , sequential([layer(nmid, nmid, act) for i in range(nl)])
-    , nn.Linear(nmid, nf)
+      nn.Linear(number_inputs, hidden_units)
+      , activation(inplace=True)
+      , sequential([layer(hidden_units, hidden_units, activation) for i in range(number_layers)])
+      , nn.Linear(hidden_units, number_outputs)
     )
 
 
@@ -156,6 +134,41 @@ def load(path, transport, adversary, toptim, aoptim):
 def tonp(xs):
   return xs.cpu().detach().numpy()
 
+
+def detailed_plots(transport, writer, epoch, device):
+
+  # -----------------------------------------------
+  # make closure plot to check that transported MC
+  # indeed matches the data
+  # -----------------------------------------------
+
+  length_data = 1000
+  toy_MC = genMC(length_data, device)
+  toy_data = genData(length_data, device)
+
+  transported_MC = tonp(toy_MC + transport(toy_MC))
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111)  
+  ax.hist([toy_MC[:, 0], toy_data[:, 0], transported_MC[:, 0]], bins=25, label=["mc", "data", "transported mc"])
+  ax.legend()
+
+  writer.add_figure("closure", fig, global_step = epoch)
+  plt.close()
+
+  # -----------------------------------------------
+  # plot the action of the transport function itself
+  # -----------------------------------------------
+
+  xvals = torch.unsqueeze(torch.linspace(-4.0, 4.0, 1000), 1)
+  yvals = tonp(xvals + transport(xvals))
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.plot(xvals, yvals)
+
+  writer.add_figure("transport function", fig, global_step = epoch)
+  plt.close()
 
 def plotPtTheta(transport, predict, targ, nps, writer, label, title, epoch, device, nmax=-1):
 
@@ -409,17 +422,9 @@ def plotPtTheta(transport, predict, targ, nps, writer, label, title, epoch, devi
 
 
 
-def trans(transport, mc, thetas):
-    tmp = transport(mc)
-    cv = tmp[:,0:1] # central value
-    var = tmp[:,1:] # eigen variations
-    coeffs = var - cv
+def trans(transport, mc):
 
-    corr = torch.bmm(thetas.unsqueeze(1), coeffs.unsqueeze(2))
-
-    return cv + corr.squeeze(2)
-
-
+    return transport(mc)
 
 def ptbin(low, high, samps):
   pts = torch.exp(samps[:,1])
